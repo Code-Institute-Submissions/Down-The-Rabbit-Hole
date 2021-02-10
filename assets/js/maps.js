062c6e740112b8245fa43b00ab03ffcd93531edf
elliemcavoy <ellie.mcavoy@outlook.com>
2021-02-10 15:38:16 +0000

    myshop and myartwork pages added, maps.js location corrected. Images added to myartwork and index.html. Layout created for myartwork page

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,18 +1,17 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 10,
         center: {
-            lat: 46.619261,
-            lng: -33.134766
+            lat: 53.796960, 
+            lng: -1.744628
         }
     });
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     var locations = [
-        { lat: 40.785091, lng: -73.968285 },
-        { lat: 41.084045, lng: -73.874245 },
-        { lat: 40.754932, lng: -73.984016 }
+        { lat: 53.822279, lng: -1.729173 },
+        
     ];
 
     var markers = locations.map(function(location, i) {
